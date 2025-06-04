983e48e44b4bf49ad1d8bcb33daf717801f4735f
meshil11 <jojo1411m@gmail.com>
2025-06-04 21:00:23 +0300

    Update bot_render.py

--- bot_render.py
+++ bot_render.py
@@ -157,4 +157,7 @@ async def main():
     await app.run_polling()
 
 if __name__ == '__main__':
+    import nest_asyncio
+    nest_asyncio.apply()
+    asyncio.get_event_loop().run_until_complete(main())
     asyncio.run(main())
